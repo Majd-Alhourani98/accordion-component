70c42bd2431a9884f4d0d2ce7e023f3ab4065523
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-08-07 20:15:42 +0300

    finish the component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
+import { useState } from "react";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
 function App() {
-  return <div>Hello </div>;
+  return <Accordion fags={faqs} />;
 }
 
+function Accordion({ fags }) {
+  return (
+    <div className="accordion">
+      {faqs.map((fag, i) => (
+        <AccordionItem number={i} title={fag.title} text={fag.text} />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ number, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((o) => !o);
+  }
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">
+        {number < 9 ? `0${number + 1}` : `${number + 1}`}
+      </p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
+}
 export default App;
